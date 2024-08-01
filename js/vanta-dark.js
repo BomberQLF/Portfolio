c0524e1c0bfef934f14c6aca7d92d4465204ddaf
BomberQLF <tmurphyparis@gmail.com>
2024-08-01 18:31:05 +0200

    Adjustements

--- js/vanta-dark.js
+++ js/vanta-dark.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
         minHeight: 200.00,
         minWidth: 200.00,
         scale: 1.00,
-        color1: 0x222222,
+        color1: 0x161616,
         color2: 0x141313,
-        size: 0.60,
+        size: 0.20,
         speed: 1.50
     });
 });
