a9d8a9b0dcad874b4c129f4276b2eabc6ab64599
BomberQLF <tmurphyparis@gmail.com>
2024-08-01 13:30:44 +0200

    Added a background
    Used Vanta.js to create an animated background for my hero section

---
+++ js/vanta-dark.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    VANTA.CELLS({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        color1: 0x2e2f2f,
+        color2: 0x141313,
+        size: 0.60,
+        speed: 1.50
+    });
+});
