df6c96eb15f39d7fc747d2f12a0dfd69cc0857a4
BomberQLF <tmurphyparis@gmail.com>
2024-08-01 18:01:28 +0200

    Save 01/08

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loadingContainer = document.querySelector('.loading_container');
-    const heroSection = document.querySelector('.hero_section');
+    const hiddenSections = document.querySelector('.hidden');
   
     // Affiche la hero_section après la fin de l'animation de chargement
     setTimeout(() => {
       loadingContainer.style.display = 'none';
-      heroSection.style.display = 'inline'; // Change le display pour afficher la section
-      heroSection.classList.add('show');
-    }, 4000); // 3000 ms correspond au temps de l'animation
+      hiddenSections.style.display = 'block'; 
+      hiddenSections.classList.add('show');
+    }, 4000);
   });
--- js/vanta-dark.js
+++ js/vanta-dark.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         minHeight: 200.00,
         minWidth: 200.00,
         scale: 1.00,
-        color1: 0x2e2f2f,
+        color1: 0x222222,
         color2: 0x141313,
         size: 0.60,
         speed: 1.50
