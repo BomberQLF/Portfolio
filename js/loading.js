9cde31878f6bb01739f4b04f6a85b720393fb8fc
BomberQLF <tmurphyparis@gmail.com>
2024-08-05 02:43:17 +0200

    Save 05/07

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loadingContainer = document.querySelector('.loading_container');
     const hiddenSections = document.querySelector('.hidden');
+    const main = document.querySelector('main');
   
     // Affiche la hero_section après la fin de l'animation de chargement
     setTimeout(() => {
       loadingContainer.style.display = 'none';
+      main.style.display = "block";
       hiddenSections.style.display = 'block'; 
       hiddenSections.classList.add('show');
     }, 4000);
