cdcd87de65700c47608110af12a04694ff106107
BomberQLF <tmurphyparis@gmail.com>
2024-10-11 00:01:45 +0200

    Save 11/10

--- js/gsap.js
+++ js/gsap.js
@@ -1,6 +1,11 @@
-
 const skills = document.querySelectorAll('.under_skills');
 
 skills.forEach((e) => {
-    console.log(e);
+    e.addEventListener('mouseover', (hover) => {
+        gsap.to(hover.target, {paddingLeft: 10, duration: 0.5, ease: "power2.out"});
+    });
+
+    e.addEventListener('mouseout', (hover) => {
+        gsap.to(hover.target, {paddingLeft: 0, duration: 0.5, ease: "power2.out"});
+    });
 });
