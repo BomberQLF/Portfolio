b4e0ea45f79990247d90d85696e096a37beca7ce
BomberQLF <tmurphyparis@gmail.com>
2024-10-17 22:17:11 +0200

    Save 17/10

--- js/gsap.js
+++ js/gsap.js
@@ -1,12 +1,15 @@
-const skills = document.querySelectorAll('.under_skills');
+gsap.registerPlugin(ScrollTrigger);
 
+const skills = document.querySelectorAll('.under_skills');
 skills.forEach((e) => {
     e.addEventListener('mouseover', (hover) => {
-        gsap.to(hover.target, {paddingLeft: 10, duration: 0.5, ease: "power2.out"});
+        gsap.to(hover.target, { paddingLeft: 10, duration: 0.5, ease: "power2.out" });
     });
 
     e.addEventListener('mouseout', (hover) => {
-        gsap.to(hover.target, {paddingLeft: 0, duration: 0.5, ease: "power2.out"});
+        gsap.to(hover.target, { paddingLeft: 0, duration: 0.5, ease: "power2.out" });
     });
 });
 
+const gridFilled = document.querySelectorAll('.grid_filled');
+// Faire un forEach qui passe sur gridFilled et qui appelle gsap avec un fromTo() en modifiant l'opacité.
