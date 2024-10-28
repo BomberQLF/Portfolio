124a163aa0a456ae2c9cac9babfade8276a4f079
BomberQLF <tmurphyparis@gmail.com>
2024-10-28 12:21:29 +0100

    Code pour cacher le footer

--- js/loading.js
+++ js/loading.js
@@ -3,30 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const hiddenSections = document.querySelector('.hidden');
   const main = document.querySelector('main');
   const footer = document.querySelector('footer');
-
-  // Vérifie si loadingContainer existe avant d'appliquer les changements
-  if (loadingContainer) {
-    // Affiche les sections après la fin de l'animation de chargement
-    setTimeout(() => {
-      loadingContainer.style.display = 'none';
-
-      // Vérifie si chaque section existe avant de modifier son style
-      if (footer) {
-        footer.style.display = "flex";
-        footer.style.flexDirection = "column";
-      }
-      if (main) main.style.display = "block";
-      if (hiddenSections) {
-        hiddenSections.style.display = 'block';
-        hiddenSections.classList.add('show');
-      }
-    }, 4000);
-  }
-});document.addEventListener('DOMContentLoaded', () => {
-  const loadingContainer = document.querySelector('.loading_container');
-  const hiddenSections = document.querySelector('.hidden');
-  const main = document.querySelector('main');
-  const footer = document.querySelector('footer');
   const introduction = document.querySelector('.introduction');
   const quote = document.querySelector('.quote');
   const gridkey = document.querySelector('.grid_key');
@@ -34,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const story = document.querySelector('.story');
   const mywork = document.querySelector('.my_work');
   const caseList = document.querySelector('.case_list');
+  const classFooter = document.querySelector('.footer_about_container');
 
   // Vérifie si loadingContainer existe avant d'appliquer les changements
   if (loadingContainer) {
@@ -57,6 +34,10 @@ document.addEventListener('DOMContentLoaded', () => {
       if (hiddenSections) {
         hiddenSections.style.display = 'block';
         hiddenSections.classList.add('show');
+      }
+      // Affiche la section footer_about_container après l'animation
+      if (classFooter) {
+        classFooter.style.display = 'flex';
       }
     }, 4000);
   }
